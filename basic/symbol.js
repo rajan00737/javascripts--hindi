2ffc57f1d7e8bcb4121a29b16c53347f60fe0c1b
rajan00737 <rajanmauryaakld@gmail.com>
2024-08-01 06:28:12 +0000

    container config done

---
+++ basic/symbol.js
@@ -0,0 +1,10 @@
+const id = Symbol('125');
+const anotherid = Symbol('125');
+
+console.log(id===anotherid);
+
+// const name = rajan;
+// const no = 7;
+
+
+// console.log(`My name is ${name} and my id no is ${no}`);
